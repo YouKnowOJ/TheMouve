81a5283c10b5caeb8ceb317a9d73889b4036b654
YouKnowOJ <ojogbo2@illinois.edu>
2015-07-06 12:07:50 -0500

    api config fixed

--- api/config.php
+++ api/config.php
@@ -4,9 +4,9 @@
      */
     
     // Consumer Key
-    define('CONSUMER_KEY', 'DWPEFvniRpu3bpj5mM3M2ZnvO');  // CONSUMER_KEY_HERE
-    define('CONSUMER_SECRET', 'zKh8DVRU2BuFhkt95VtyS04uFAr2w8MqcqtZGkKMoK1iQ9PlAd'); // CONSUMER_SECRET_HERE
+    define('CONSUMER_KEY', 'Tep6B38Iy83neuJUrbfdb5Q4R');  // CONSUMER_KEY_HERE
+    define('CONSUMER_SECRET', 'Y6BxfatgODD9fHOcxlry4TYBb53BcHvTesea9QfCfTAa5ufDtX'); // CONSUMER_SECRET_HERE
 
     // User Access Token
-    define('ACCESS_TOKEN', '2777997085-8tn8nTntaEfTRBIsZ0V22I76AUIkYqVFuUnv3Bw'); // ACCESS_TOKEN_HERE
-    define('ACCESS_SECRET', 'AZtonLazfNixH76Bma0lAJbnvrROiBfxjfsVFxFkHF1jR'); // ACCESS_SECRET_HERE
+    define('ACCESS_TOKEN', '2252410417-65fFsUD4901xBEjoxzcHa83FlLDuYGvociIuu5V'); // ACCESS_TOKEN_HERE
+    define('ACCESS_SECRET', 'qs3vxpCzwYDQGCfl0Q2FbbFo0ZpvfWOGeaQh7A2EBSXHG'); // ACCESS_SECRET_HERE
